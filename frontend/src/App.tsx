a9e476b746285cd56be7475187d3366946c32fe9
Itay Moav <itay.malimovka@gmail.com>
2024-08-26 21:37:02 -0400

    settled on this , wails go and ts are the new things here

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
-import wailsLogo from './assets/wails.png'
-import './App.css'
+import {useState} from 'react';
+import logo from './assets/images/logo-universal.png';
+import './App.css';
+import {Greet} from "../wailsjs/go/main/App";
 
 function App() {
+    const [resultText, setResultText] = useState("Please enter your name below 👇");
+    const [name, setName] = useState('');
+    const updateName = (e: any) => setName(e.target.value);
+    const updateResultText = (result: string) => setResultText(result);
+
+    function greet() {
+        Greet(name).then(updateResultText);
+    }
+
     return (
-        <div className="min-h-screen bg-white grid grid-cols-1 place-items-center justify-items-center mx-auto py-8">
-            <div className="text-blue-900 text-2xl font-bold font-mono">
-                <h1 className="content-center">Vite + React + TS + Tailwind</h1>
-            </div>
-            <div className="w-fit max-w-md">
-                <a href="https://wails.io" target="_blank">
-                    <img src={wailsLogo} className="logo wails" alt="Wails logo" />
-                </a>
+        <div id="App">
+            <img src={logo} id="logo" alt="logo"/>
+            <div id="result" className="result">{resultText}</div>
+            <div id="input" className="input-box">
+                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
+                <button className="btn" onClick={greet}>Greet</button>
             </div>
         </div>
     )
